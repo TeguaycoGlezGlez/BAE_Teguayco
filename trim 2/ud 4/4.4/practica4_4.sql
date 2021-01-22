57403d0d14b8a4118667699debab5b6c16a1eb2c
teguayco <tegua.glez2@gmail.com>
2021-01-22 19:07:21 +0000

    añadido ejercicio 4.3

---
+++ trim 2/ud 4/4.4/practica4_4.sql
@@ -0,0 +1,39 @@
+-- Se dispone de la tabla ALUMNOS con los datos de aquellos alumnos matriculados en un instituto. Realizar las siguientes consultas SQL:
+
+1. Muestra todos los datos de todos los alumnos.
+2. Muestra el nombre , localidad y fecha de nacimiento de todos los alumnos.
+3. Muestra el nombre de todos los alumnos. En el resultado de la consulta, la columna “Nombre” debe aparecer con la etiqueta “Nombre y apellidos” (ALIAS DE COLUMNAS).
+4. Obtén el nombre y el número de faltas multiplicado por 2 de todos los alumnos (COLUMNAS CALCULADAS). En el resultado de la consulta, la columna faltas debe aparecer con la etiqueta “Faltas de asistencia” (ALIAS DE COLUMNAS)
+5. Consulta los datos de los alumnos que son de Lorca.
+6. Obtén los datos de los alumnos que son de Murcia o Alhama de Murcia.
+7. Obtén los datos de los alumnos que son de Murcia y están en el primer curso de E.S.O.
+8. Muestra los alumnos que son de Lorca, están en segundo curso de Bachillerato y tienen más de 10 faltas.
+9. Obtén los datos de aquellos alumnos que son de Murcia ordenados por nombre.
+10. Obtén los datos de todos los alumnos ordenados por nivel, y dentro de cada nivel por curso.
+  
+11. Muestra los datos de aquellos alumnos que tengan más de 10 faltas en primer o segundo curso.
+12. Muestra los datos de todos aquellos alumnos que tengan menos de 10 faltas en 3o o 4o curso y además sean de Murcia.
+13. Obtén con una consulta todos los cursos que hay sin repeticiones (DISTINCT).
+14. Obtén los datos de los alumnos que no tengan 10 faltas en 1o de E.S.O.
+15. Muestra los datos de aquellos alumnos cuyo nombre empiece por la letra 'B'.
+16. Con una consulta obtén los alumnos que son de Murcia y cuyo nombre termina con una letra 'O'.
+17. Muestra los datos de todos aquellos alumnos que están en 1o curso de E.S.O. Y tienen una letra 'U' en la segunda posición del nombre.
+18. Obtén los datos de los alumnos cuya columna “Faltas” es nula.
+19. Muestra los datos de aquellos alumnos que tienen entre 10 y 20 faltas (BETWEEN). Ordena el resultado por nombre.
+20. Muestra los datos de los alumnos que tienen entre 10 y 20 faltas y además son de Murcia.
+21. Muestra los datos de los alumnos que tienen entre 10 y 20 faltas, son de Murcia y están matriculados en 1o de E.S.O.
+22. Con una consulta muestra los datos de los alumnos cuyas faltas sean menores que 10 y mayores que 20.
+23. Muestra los datos de los alumnos cuya fecha de nacimiento comprenda los años 1993 y 1994. Ordena el resultado por nombre.
+24. Muestra los datos de los alumnos que sean de primer o segundo curso (no importa que sean de E.S.O. O Bachiller). Utiliza el operador IN.
+25. Obtén los datos de aquellos alumnos que sean de tercer o cuarto curso y sean de Murcia. Utiliza el operador IN.
+26. Muestra los datos de los alumnos que no sean de E.S.O, ordenados por curso y por nombre descendentemente. Utiliza el operador IN.
+27. Muestra los datos de los alumnos que sean de primer o segundo curso y no sean de Bachiller. Ordena el resultado por nombre.
+ 
+28. Obtén los datos de los alumnos cuyo nombre empiece por la letra 'J', que tengan más de 10 faltas y no sean de Bachiller. Ordena el resultado por curso, y dentro de cada curso, por nombre.
+29. Con una consulta obtén el expediente, nombre, curso y nivel de todos los alumnos ordenado por curso, nivel ascendentemente y nombre descendentemente cuyo nivel no sea 'BACHILLER'.
+30. Con una consulta obtén el nombre de cada alumno en mayúscula cuya localidad sea Murcia.
+31. Muestra el nombre de cada alumno en mayúscula y la localidad a la que pertenecen en minúscula, ordenados por localidad.
+32. Con una consulta concatena el nombre de cada alumno y la localidad y sustituye 'BACHILLER' por 'Bachillerato'.
+33. Con una consulta obtén el nombre de cada alumno y la longitud de la cadena “Nombre”.
+34. Con una consulta obtén el nombre, año de nacimiento y mes de nacimiento de cada uno de los alumnos.
+35. Con una consulta muestra el nombre y edad de cada uno de los alumnos (la edad se calcula restando al año de nacimiento 2008)
